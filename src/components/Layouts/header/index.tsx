eeab81c9af2d3ce3c82ad10715e7856bf33ec04e
Katie H <k.hartrick@ecu.edu.au>
2025-07-31 10:50:41 +0800

    Update index.tsx

--- src/components/Layouts/header/index.tsx
+++ src/components/Layouts/header/index.tsx
@@ -36,7 +36,7 @@ export function Header() {
 
       <div className="max-xl:hidden">
         <h1 className="mb-0.5 text-heading-5 font-bold text-dark dark:text-white">
-          Dashboard
+          Digital Marketing, Experience and Analytics Team
         </h1>
         <p className="font-medium">Rebuild of ECU web tools using Next.js</p>
       </div>
