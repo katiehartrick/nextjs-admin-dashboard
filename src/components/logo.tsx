1ba162bd27c3ea984540ae579ea4b1d943c8bf15
Katie H <k.hartrick@ecu.edu.au>
2025-07-31 10:24:18 +0800

    add ECU logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,24 +1,24 @@
-import darkLogo from "@/assets/logos/dark.svg";
-import logo from "@/assets/logos/main.svg";
 import Image from "next/image";
 
 export function Logo() {
   return (
     <div className="relative h-8 max-w-[10.847rem]">
       <Image
-        src={logo}
-        fill
+        src="https://www.ecu.edu.au/__data/assets/image/0004/1100389/ecu-logo.png"
         className="dark:hidden"
-        alt="NextAdmin logo"
+        width={88}
+        height={50}
+        alt="Edith Cowan University logo"
         role="presentation"
         quality={100}
       />
 
       <Image
-        src={darkLogo}
-        fill
+        src="https://www.ecu.edu.au/__data/assets/image/0014/1100390/ecu-logo-alternate.png"
         className="hidden dark:block"
-        alt="NextAdmin logo"
+        width={88}
+        height={50}
+        alt="Edith Cowan University logo"
         role="presentation"
         quality={100}
       />
