0609e36878c70a595a7ffc03cca84c1509d21c15
Katie H <k.hartrick@ecu.edu.au>
2025-07-31 10:38:00 +0800

    update logo size

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export function Logo() {
   return (
-    <div className="relative h-8 max-w-[10.847rem]">
+    <div className="relative">
       <Image
         src="https://www.ecu.edu.au/__data/assets/image/0004/1100389/ecu-logo.png"
         className="dark:hidden"
         width={88}
-        height={50}
+        height={69}
         alt="Edith Cowan University logo"
         role="presentation"
         quality={100}
@@ -17,7 +17,7 @@ export function Logo() {
         src="https://www.ecu.edu.au/__data/assets/image/0014/1100390/ecu-logo-alternate.png"
         className="hidden dark:block"
         width={88}
-        height={50}
+        height={69}
         alt="Edith Cowan University logo"
         role="presentation"
         quality={100}
