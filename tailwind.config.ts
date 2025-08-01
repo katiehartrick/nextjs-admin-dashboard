7285766eead3dcdf4859e87bfc3081f2296863d3
Katie H <k.hartrick@ecu.edu.au>
2025-08-01 15:50:23 +0800

    add ECU colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,55 @@ const config: Config = {
       colors: {
         current: "currentColor",
         transparent: "transparent",
-        white: "#FFFFFF",
+        // White variants
+        'white': 'hsl(0deg 0 100% / 100%)', // #ffffff
+        'ecru': 'hsl(156deg 19% 95% / 100%)', // #eff4f2
+
+        // Teal variants, primary colour
+        'teal': {
+          50: 'hsl(169deg 48% 90% / 100%)', // #daf2ee
+          100: 'hsl(169deg 47% 81% / 100%)', // #b6e5dc
+          200: 'hsl(169deg 47% 71% / 100%)', // #92d8cb
+          300: 'hsl(169deg 48% 62% / 100%)', // #6eccbb
+          400: 'hsl(169deg 47% 52% / 100%)', // #4abea9
+          500: 'hsl(169deg 65% 42% / 100%)', // #26b298
+          600: 'hsl(169deg 64% 35% / 100%)', // #20947f
+          700: 'hsl(169deg 65% 28% / 100%)', // #197766
+          800: 'hsl(169deg 65% 21% / 100%)', // #13594c
+          900: 'hsl(169deg 64% 14% / 100%)', // #0d3c33
+          DEFAULT: 'hsl(169deg 65% 42% / 100%)' // 500
+        },
+
+        // Plum variants, secondary colour
+        'plum': {
+          50: 'hsl(324deg 49% 90% / 100%)', // #f2d9e8
+          100: 'hsl(324deg 48% 80% / 100%)', // #e4b3d0
+          200: 'hsl(324deg 49% 70% / 100%)', // #d88eba
+          300: 'hsl(324deg 49% 60% / 100%)', // #cb68a3
+          400: 'hsl(324deg 49% 50% / 100%)', // #be428c
+          500: 'hsl(324deg 72% 40% / 100%)', // #b11d75
+          600: 'hsl(324deg 72% 32% / 100%)', // #941862
+          700: 'hsl(324deg 72% 27% / 100%)', // #76134e
+          800: 'hsl(324deg 71% 20% / 100%)', // #590f3b
+          900: 'hsl(324deg 71% 14% / 100%)', // #3b0a27
+          DEFAULT: 'hsl(324deg 72% 40% / 100%)' // 500
+        },
+
+        // Black variants, tertiary colour
+        'black': {
+          50: 'hsl(0deg 0 90% / 100%)', // #e5e5e5
+          100: 'hsl(0deg 0 80% / 100%)', // #ccc
+          200: 'hsl(0deg 0 70% / 100%)', // #b2b2b2
+          300: 'hsl(0deg 0 60% / 100%)', // #999
+          400: 'hsl(0deg 0 50% / 100%)', // #7f7f7f
+          500: 'hsl(206deg 0 40% / 100%)', // #666
+          600: 'hsl(0deg 0 30% / 100%)', // #4d4d4d
+          700: 'hsl(0deg 0 20% / 100%)', // #333
+          800: 'hsl(0deg 0 10% / 100%)', // #1a1a1a
+          900: 'hsl(0deg 0 0 / 100%)', // #000
+          DEFAULT: 'hsl(0deg 0 0 / 100%)' // 900
+        },
+
         primary: "#5750F1",
         stroke: "#E6EBF1",
         "stroke-dark": "#27303E",
